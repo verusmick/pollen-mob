f6b606eb7a2b32102197b705d04acb37fecaa373
Miguel Angel <miguel.angel.angulo72@gmail.com>
2025-04-23 17:44:25 -0400

    Create aliase to deploy the project always in the same  URL

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ https://docs.expo.dev/guides/publishing-websites/
 https://github.com/CocoaPods/CocoaPods/issues/12145#issuecomment-2167668416
 
 https://stackoverflow.com/a/79086359
+
+
+
+
+Export your app
+You need to export your web project into a dist directory. To do this, run:
+
+Terminal
+
+Copy
+
+npx expo export --platform web
+eas deploy --alias staging
--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,8 @@ import { View, Text } from 'react-native'
 const App = () => {
   return (
     <View className='mt-20'>
-      <Text className='text-3xl font-bold text-blue-500'>Hello World</Text>
+      <Text className='text-3 xl font-bold text-blue-500'> PollenScience.eu</Text>
+      <Text className='text-3 xl font-bold text-red-950'> PollenScience.eu</Text>
     </View>
   )
 }
