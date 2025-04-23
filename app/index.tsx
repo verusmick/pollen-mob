71056019ca30c4e73b03ff73cda1f44de874b265
Miguel Angel <miguel.angel.angulo72@gmail.com>
2025-04-23 17:13:59 -0400

    Set up Nativewind in the project

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <View className='mt-20'>
+      <Text className='text-3xl font-bold text-blue-500'>Hello World</Text>
+    </View>
+  )
+}
+
+export default App
